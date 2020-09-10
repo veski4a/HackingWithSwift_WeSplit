6b9c38eb022ed5a74948a0e69374a16c79f10cf9
Veselin Stefanov <veski4a92@gmail.com>
2020-09-10 08:25:15 +0300

    Wrap up the app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,16 +15,52 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        return totalCheckAmount / peopleCount
+    }
+    
+    var totalCheckAmount: Double{        
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
+    
     var body: some View {
-        Form{
-            Section{
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2 ..< 100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")){
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(0 ..< tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Amount per person")){
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Total check amount")){
+                    Text("$\(totalCheckAmount, specifier: "%.2f")")
+                }
             }
-            
-            Section{
-                Text("$\(checkAmount)")
-            }
+            .navigationBarTitle("WeSplit")
         }
     }
 }
