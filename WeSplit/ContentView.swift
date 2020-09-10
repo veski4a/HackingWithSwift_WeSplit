4caa96413749b08551c59e732ad60d89f5abe9c7
Veselin Stefanov <veski4a92@gmail.com>
2020-09-10 08:30:54 +0300

    Number of people is now a text field rather than a picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double{
-        let peopleCount = Double(numberOfPeople + 2)
-        return totalCheckAmount / peopleCount
+        let peopleCount = Double(numberOfPeople) ?? 0
+        return peopleCount > 0 ? totalCheckAmount / peopleCount : 0.0
     }
     
-    var totalCheckAmount: Double{        
+    var totalCheckAmount: Double{
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
@@ -36,11 +36,8 @@ struct ContentView: View {
                 Section{
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
-                    Picker("Number of people", selection: $numberOfPeople){
-                        ForEach(2 ..< 100){
-                            Text("\($0) people")
-                        }
-                    }
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
                 }
                 
                 Section(header: Text("How much tip do you want to leave?")){
